4ceda19f64d2d26127bfd883545f8f68eb97b2d9
zhangj83 <jitao_david.zhang@roche.com>
2014-05-29 16:01:41 +0200

    do_wmw_test has been refactored into wmw_test, which makes the function running much faster (<60s for 2000 samples in a single core!)

--- src/wmw_test_R.c
+++ src/wmw_test_R.c
@@ -14,85 +14,83 @@
 #define NROW(x) INTEGER(GET_DIM((x)))[0]
 #define NCOL(x) INTEGER(GET_DIM((x)))[1]
 
-double do_wmw_test(const int* indices,
-		   int n1,
-		   const double* stats,
-		   int n,
-		   int val_type /* 0=U, 1=p(less), 2=p(right) */ ) {
-
-  int i,j;
-  int n2=n-n1;
-  double irsum=0; // sum of rank of index
-  static double tmp;
-  double U, mu, sigma2,zlt, zut, plt, pgt;
-  int ulen=0;
-
-  DRankList list=createDRankList(stats, n);
-  rankDRankList(list);
-
-  // sum of the ranks of indexed elements
-  for(i=0;i<n1;++i)
-    irsum+=list->list[ indices[i] ]->rank;
-  ulen=list->ulen;
-
-  U=n1*n2+n1*(n1+1.0)*0.5-irsum; 
-  if(val_type==0) {
-    destroyDRankList(list);
-    return(U);
-  }
-  
-  mu=(double)n1*n2*0.5;
-  sigma2=n1*n2*(n+1.0)/12;
-  
-  if(ulen!=n) { // has ties
-    int* tbl=(int*)malloc(ulen * sizeof(int));
-    int ncount=0;
-    double prod=0;
-    sortDRankList(list);
-    for(i=0;i<n;i=j+1) {
-      j=i;
-      while(j<n-1 && (*(list->list[j+1]->vPtr)==*(list->list[j]->vPtr))) ++j;
-      tbl[ncount++]=j-i+1;
-    }
-    for(i=0;i<ulen;++i)
-      prod+=(0.0+tbl[i])/n*(tbl[i]+1)/(n+1)*(tbl[i]-1)/(n-1);
-    sigma2*=(1-prod);
-    free(tbl);
-  }
-
-  destroyDRankList(list);
-
-  if(val_type==1) {
-    zut=(U-0.5-mu)/sqrt(sigma2);
-    pnorm_both(zut, &tmp, &plt, 1, 0);
-    return(plt);
-  } else if (val_type==2) {
-    zlt=(U+0.5-mu)/sqrt(sigma2);
-    pnorm_both(zlt, &pgt, &tmp, 0, 0);
-    return(pgt);
-  } else {
-    error("Should not happen");
-    return(-1);
-  }
-}
-		   
 SEXP wmw_test(SEXP indlist, SEXP matrix, SEXP val_type) {
-  int i,j, ilen,k;
+  int i,j, n1,n2, k, m;
   int *ip;
-  int type=INTEGER(val_type)[0];
+  const int type=INTEGER(val_type)[0];
   const int slen=NROW(matrix);
   double *sp=REAL(matrix);
   SEXP res;
+
+  DRankList list;
+  double irsum; // sum of rank of index
+  double tmp;
+  double U;
+  double mu, sigma2;
+  double zlt, zut, plt, pgt;
+
+  // for ties
+  double tiecoef;
+
   res=PROTECT(allocMatrix(REALSXP,
 			  length(indlist), 
 			  NCOL(matrix)));
   double *resp=REAL(res);
+  
   for(i=0; i<NCOL(matrix);++i) {
+    list=createDRankList(sp, slen);    
+    rankDRankList(list);
+
+    if(list->ulen!=slen) {
+      int* tbl=(int*)malloc(list->ulen * sizeof(int));
+      int ncount=0;
+      tiecoef=0;
+      sortDRankList(list);
+      for(k=0;k<slen;k=m+1) {
+	m=k;
+	while(m<slen-1 && (*(list->list[m+1]->vPtr)==*(list->list[m]->vPtr))) ++m;
+	tbl[ncount++]=m-k+1;
+      }
+      for(k=0;k<list->ulen;++k)
+	tiecoef+=(0.0+tbl[k])/slen*(tbl[k]+1)/(slen+1)*(tbl[k]-1)/(slen-1);
+      tiecoef=1-tiecoef;
+      free(tbl);
+      rankDRankList(list);
+    } else {
+      tiecoef=1;
+    }
+
     for(j=0;j<length(indlist);++j) {
       ip=INTEGER(VECTOR_ELT(indlist,j));
-      ilen=length(VECTOR_ELT(indlist,j));
-      resp[j+i*length(indlist)]=do_wmw_test(ip, ilen, sp, slen, type); // notice the pointer should be j+i*length(indlist) [row count + column count * nrow]
+      n1=length(VECTOR_ELT(indlist,j));
+      n2=slen-n1;
+      irsum=0.0;
+      for(k=0;k<n1;++k) 
+	irsum+=list->list[ip[k]]->rank;
+
+      U=n1*n2+n1*(n1+1.0)*0.5-irsum; 
+      if(type==0) {
+	resp[j+i*length(indlist)]=U;
+      } else {
+	mu=(double)n1*n2*0.5; // mu=n1*n2*0.5
+	sigma2=n1*n2*(slen+1.0)/12*tiecoef; //sigma2 = n1*n2*(n+1)/12*tiecoef
+
+	if(type==1) {
+	  zut=(U-0.5-mu)/sqrt(sigma2);
+	  pnorm_both(zut, &tmp, &plt, 1, 0);
+	  resp[j+i*length(indlist)]=plt;
+	} else if (type==2) {
+	  zlt=(U+0.5-mu)/sqrt(sigma2);
+	  pnorm_both(zlt, &pgt, &tmp, 0, 0);
+	  resp[j+i*length(indlist)]=pgt;
+	} else {
+	  Rprintf("Unrocognized val_type. Should not happen\n");
+	}
+      }
+      //resp[j+i*length(indlist)]=do_wmw_test(ip, n1, sp, slen, type); 
     }
+
+    destroyDRankList(list);
     sp+=NROW(matrix);
   }
   
