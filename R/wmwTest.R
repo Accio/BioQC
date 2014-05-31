4b03a773a3c5a789f34f48344396c9ba235ad9a8
zhangj83 <jitao_david.zhang@roche.com>
2014-05-31 22:10:18 +0200

    clean-up documentations

--- R/wmwTest.R
+++ R/wmwTest.R
@@ -5,74 +5,87 @@ wmw.test <- function(x, sub, alternative=c("two.sided", "less", "greater"), stat
   return(ifelse(statistic, wt$statistic, wt$p.value))
 }
 
-setGeneric("wmwTest",function(object, sub, alternative, statistic) standardGeneric("wmwTest"))
-setMethod("wmwTest", signature=c("numeric", "logical", "character", "logical"),
-          function(object, sub, alternative,statistic) {
-            wmw.test(object, sub, alternative=alternative, statistic=statistic)
-          })
-setMethod("wmwTest", signature=c("numeric", "numeric", "character", "logical"),
-          function(object, sub, alternative,statistic) {
-            sel <- rep(FALSE, length(object))
-            sel[sub] <- TRUE
-            wmw.test(object, sel, alternative=alternative, statistic=statistic)
-          })
-setMethod("wmwTest", signature=c("matrix", "logical", "character", "logical"),
-          function(object, sub, alternative,statistic) {
-            apply(object, 2L, wmw.test, sub=sub, alternative=alternative, statistic=statistic)
-          })
-setMethod("wmwTest", signature=c("matrix", "numeric", "character", "logical"),
-          function(object, sub, alternative, statistic) {
-            sel <- rep(FALSE, nrow(object))
-            sel[sub] <- TRUE
-            apply(object, 2L, wmw.test, sub=sel, alternative=alternative, statistic=statistic)
-          })
+wmwTestC <- function(x, sub, alternative=c("two.sided", "less", "greater"), statistic=FALSE) {
+  alternative <- match.arg(alternative)
+  if(!any(sub)) return(ifelse(statistic, 0, 1))
+  if(statistic) {
+    val <- 3L
+  } else if (alternative=="less") {
+    val <- 1L
+  } else if (alternative=="greater") {
+    val <- 0L
+  } else if (alternative=="two.sided") {
+    val <- 2L
+  } else {
+    stop("Should not happen")
+  }
+  return(.Call("wmw_test", which(sub)-1L, matrix(x, ncol=1L), val=val))
+}
 
-setMethod("wmwTest", signature=c("ExpressionSet", "logical", "character", "logical"),
-          function(object, sub, alternative,statistic) {
-            apply(exprs(object), 2L, wmw.test, sub=sub, alternative=alternative, statistic=statistic)
-          })
-setMethod("wmwTest", signature=c("ExpressionSet", "numeric", "character", "logical"),
-          function(object, sub, alternative, statistic) {
-            sel <- rep(FALSE, nrow(object))
-            sel[sub] <- TRUE
-            apply(exprs(object), 2L, wmw.test, sub=sel, alternative=alternative, statistic=statistic)
-          })
+wmwTestCind <- function(matrix, ind.list, alternative=c("greater", "less", "greater", "U")) {
+  indC <- lapply(ind.list, function(x) {
+    if(is.logiacl(x) && length(x)==nrow(matrix)) {
+      x[is.na(x)] <- FALSE
+      return(which(x)-1L)
+    } else if(is.numeric(x)) {
+      x <- as.integer(x[!is.na(x)])
+      return(x-1L)
+    } else {
+      stop("index must be either logical or integer vector")
+    }
+  })
+  if(!is.matrix(matrix) || !is.numeric(matrix))
+    stop("matrix must be a numeric matrix")
+  type <- match.arg(type)
+  if(type=="greater") {
+    val <- 0L
+  } else if (type=="less") {
+    val <- 1L
+  } else if (type=="two.sided") {
+    val <- 2L
+  } else if (type=="U") {
+    val <- 3L
+  } else {
+    stop("Should not happen")
+  }
+  Cres <- .Call("wmw_test", indC, matrix, val)
+}
 
-setMethod("wmwTest", signature=c("ExpressionSet", "list", "character", "logical"),
-          function(object, sub, alternative, statistic) {
-            isGmtList <- all(sapply(sub, function(x) all(c("genes", "name") %in% names(x))))
-            if(!isGmtList) {
-              res.raw <- lapply(sub, function(x) wmwTest(object=object, sub=x, alternative=alternative, statistic=statistic))
-              res.names <- names(sub)
-            } else {
-              if(!"GeneSymbol" %in% colnames(fData(object))) stop("ExpressionSet must has 'GeneSymbol' as fData column\n")
-              gb <- as.character(fData(object)[,"GeneSymbol"])
-              oexp <- exprs(object)
-              myfun <- function(x) wmwTest(oexp, gb %in% x$genes, alternative=alternative, statistic=statistic)
-              
-              if(suppressMessages(require(multicore))) {
-                res.raw <- mclapply(sub, myfun)
-              } else {
-                res.raw <- lapply(sub,myfun)
-              }
-              res.names <- sapply(sub, function(x) x$name)
-            }
-            res <- data.matrix(do.call(rbind, res.raw))
-            rownames(res) <- res.names
-            colnames(res) <- sampleNames(object)
-            return(res)
-          })
-
-## short-cuts for default values
-setMethod("wmwTest", signature=c("ANY", "ANY", "ANY", "missing"),
-          function(object, sub, alternative, statistic) {
-            wmwTest(object, sub, alternative, statistic=FALSE)
-          })
-setMethod("wmwTest", signature=c("ANY", "ANY", "missing", "ANY"),
-          function(object, sub, alternative, statistic) {
-            wmwTest(object, sub, alternative="two.sided", statistic)
-          })
-setMethod("wmwTest", signature=c("ANY", "ANY", "missing", "missing"),
-          function(object, sub, alternative, statistic) {
-            wmwTest(object, sub, alternative="two.sided", statistic=FALSE)
-          })
+##setGeneric("wmwTest",function(object, sub, alternative, statistic) standardGeneric("wmwTest"))
+setGeneric("wmwTest",function(exprs, index, alternative) standardGeneric("wmwTest"))
+setMethod("wmwTest", signature=c("matrix", "numeric", "character") , function(exprs, index, alternative) {
+  wmwTestCind(exprs, list(index), alternative=alternative)
+})
+setMethod("wmwTest", signature=c("matrix", "logical", "character"), function(exprs, index, alternative) {
+  wmwTestCind(exprs, list(index), alternative=alternative)
+})
+setMethod("wmwTest", signature=c("numeric", "numeric", "character") , function(exprs, index, alternative) {
+  wmwTestCind(matrix(exprs, ncol=1), list(index), alternative=alternative)
+})
+setMethod("wmwTest", signature=c("numeric", "logical", "character") , function(exprs, index, alternative) {
+  wmwTestCind(matrix(exprs, ncol=1), list(index), alternative=alternative)
+})
+setMethod("wmwTest", signature=c("ANY", "ANY", "missing"), function(exprs, index, alternative) {
+  wmwTestCind(exprs, index, alternative="greater")
+})
+setMethod("wmwTest", signature=c("matrix", "list", "character"), function(exprs, index, alternative) {
+  res <- wmwTestCind(exprs, index, alternative=alternative)
+  colnames(res) <- colnames(exprs)
+  rownames(res) <- names(index)
+  return(res)
+})
+setMethod("wmwTest", signature=c("eSet", "list", "character"), function(exprs, index, alternative) {
+  wmwTestCind(exprs(exprs), index, alternative=alternative)
+})
+setMethod("wmwTest", signature=c("eSet", "gmtlist", "character"), function(exprs, index, alternative) {
+  if(!"GeneSymbol" %in% colnames(fData(object)))
+    stop("ExpressionSet must has 'GeneSymbol' as fData column which contains gene symbols used in the GMT files\n")
+  gb <- as.character(fData(object)[,"GeneSymbol"])
+  ind <- lapply(index, function(x) {
+    rind <- match(x$genes, gb)
+    rind <- rind[!is.na(rind)]
+    return(rind-1L)
+  })
+  names(ind) <- sapply(index, function(x) x$name)
+  wmwTest(exprs(exprs), ind, alternative=alternative)
+})
--- tests/gmt.R
+++ tests/gmt.R
@@ -1,43 +1,43 @@
 library(BioQC)
-
-gsItem1 <- gsitem("GeneSet1", "Desc", c("G1", "G2", "G6"))
-gsItem2 <- gsitem("GeneSet2", "Desc 2", c("G2", "G3", "G4", "G5", "G6", "G7"))
-gsItem3 <- gsitem("GeneSet3", "Desc 3", c("G4", "G5", "G6"))
-gsItem4 <- gsitem("GeneSet4", "Desc 4", "G6")
-
-gss <- list(gsItem1, gsItem2, gsItem3, gsItem4)
-gsList <- as(gss, "gslist")
-
-show(gsItem1)
-show(gsItem2)
-show(gsItem3)
-show(gsItem4)
-show(gsList)
-
-gsGenes(gsItem1)
-gsGenes(gsItem2)
-gsGenes(gsItem3)
-gsGenes(gsItem4)
-gsGenes(gsList)
-gsUniqGenes(gsList)
-
-(gsDf <- as.data.frame(gsList))
-as(gsList, "data.frame")
-
-(gsMat <- as.matrix(gsList))
-as(gsMat, "matrix")
-
-tfidf(gsList)
-gsListTFIDF <- gsTfIdf(gsList)
-gsWeights(gsListTFIDF)
-
-gsList2 <- gsList
-print(gsList2)
-gsWeights(gsList2) <- list(2,2,2,2)
-gsWeights(gsList2)
-gsWeights(gsList3 <- gsClearWeights(gsList2))
-
-gsHasWeights(gsList)
-gsHasWeights(gsList2)
-gsHasWeights(gsList3)
-gsHasWeights(gsListTFIDF)
+##
+##gsItem1 <- gsitem("GeneSet1", "Desc", c("G1", "G2", "G6"))
+##gsItem2 <- gsitem("GeneSet2", "Desc 2", c("G2", "G3", "G4", "G5", "G6", "G7"))
+##gsItem3 <- gsitem("GeneSet3", "Desc 3", c("G4", "G5", "G6"))
+##gsItem4 <- gsitem("GeneSet4", "Desc 4", "G6")
+##
+##gss <- list(gsItem1, gsItem2, gsItem3, gsItem4)
+##gsList <- as(gss, "gslist")
+##
+##show(gsItem1)
+##show(gsItem2)
+##show(gsItem3)
+##show(gsItem4)
+##show(gsList)
+##
+##gsGenes(gsItem1)
+##gsGenes(gsItem2)
+##gsGenes(gsItem3)
+##gsGenes(gsItem4)
+##gsGenes(gsList)
+##gsUniqGenes(gsList)
+##
+##(gsDf <- as.data.frame(gsList))
+##as(gsList, "data.frame")
+##
+##(gsMat <- as.matrix(gsList))
+##as(gsMat, "matrix")
+##
+##tfidf(gsList)
+##gsListTFIDF <- gsTfIdf(gsList)
+##gsWeights(gsListTFIDF)
+##
+##gsList2 <- gsList
+##print(gsList2)
+##gsWeights(gsList2) <- list(2,2,2,2)
+##gsWeights(gsList2)
+##gsWeights(gsList3 <- gsClearWeights(gsList2))
+##
+##gsHasWeights(gsList)
+##gsHasWeights(gsList2)
+##gsHasWeights(gsList3)
+##gsHasWeights(gsListTFIDF)
