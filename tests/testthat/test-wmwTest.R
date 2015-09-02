104b605b43ec65023b172e4f324a308633a14631
Jitao David Zhang <jitao_david.zhang@roche.com>
2015-09-02 08:43:43 +0200

    testthat tests

--- tests/testthat/test-wmwTest.R
+++ tests/testthat/test-wmwTest.R
@@ -1,13 +1,17 @@
-library(BioQC)
-NROW <- 2000
-NCOL <- 20
-GSCOUNT <- 5
+## test wmwTest
+
+NROW <- 100
+NCOL <- 40
+GSCOUNT <- 20
 GSSIZE <- sapply(1:GSCOUNT, function(x) sample(1:NROW/2, replace=TRUE))
 set.seed(1887)
 ind <- lapply(1:GSCOUNT, function(i) sample(1:NROW, GSSIZE[i]))
 exprs <- matrix(round(rnorm(NROW*NCOL),4), nrow=NROW)
+
 gc()
-system.time(Cres <- wmwTest(exprs, ind, alternative="less"))
+system.time(Cless <- wmwTest(exprs, ind, alternative="less"))
+system.time(Cgreater <- wmwTest(exprs, ind, alternative="greater"))
+system.time(Ctwosided <- wmwTest(exprs, ind, alternative="two.sided"))
 wmwTestR <- function(matrix, index, alternative, stat) {
   sub <- rep(FALSE, length(matrix))
   sub[index]=TRUE
@@ -23,21 +27,10 @@ system.time(Rtwosided <- apply(exprs, 2, function(x)
                               sapply(ind, function(y)
                                      wmwTestR(x, y, alternative="two.sided", stat=FALSE))))
 
-testWMW <- function(ind.list, matrix, alternative=c("less", "greater", "two.sided")) {
-  alternative <- match.arg(alternative)
-  system.time(Cres <- wmwTest(matrix, ind.list, alternative=alternative))
-  system.time(Rres <- apply(matrix, 2, function(x)
-                            sapply(ind.list, function(y)
-                                   wmwTestR(x, y, alternative=alternative, stat=FALSE))))
-  if(!all(abs(Cres - Rres)<1E-5)) {
-    cat("ERROR\n")
-    print(Cres)
-    print(Rres)
-    print(summary(as.vector(Cres-Rres)))
-    quit(save="no", status=1)
-  }
-}
+expect_equal(Cless, Rless, tolerance=1E-8)
+expect_equal(Cgreater, Rgreater,tolerance=1E-8)
+## note that sometimes this fails when the p value of R is 1 whiel of C is around 0.996
+expect_equal(Ctwosided, Rtwosided, tolerance=1E-8)
 
-testWMW(ind, exprs, "less")
-testWMW(ind, exprs, "greater")
-testWMW(ind, exprs, "two.sided")
+
+## wmwTest(exprs[ind[[19]],16],exprs[,16]) ## reports segmentation error - this shuold not happen (some sanity check is missing)
