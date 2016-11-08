b0fbe1eb46601ec0236c3b3f761be5904b706a1e
zhangj83 <jitao_david.zhang@roche.com>
2016-11-08 16:19:18 +0100

    gini_matrix replaces gini

--- src/bioqc.c
+++ src/bioqc.c
@@ -2,7 +2,7 @@
 #include "bioqc.h"
 
 static const R_CallMethodDef callMethods[] = {
-  CALLMETHOD_DEF("gini", 1),
+  CALLMETHOD_DEF("gini_matrix", 3),
   CALLMETHOD_DEF("read_gmt", 1),
   CALLMETHOD_DEF("wmw_test", 3),
   CALLMETHOD_DEF("signed_wmw_test", 3),
