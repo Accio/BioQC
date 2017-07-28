4350aa9a3c1568575bc407b0cf60133674bc5d75
Jitao David Zhang <davidvonpku@gmail.com>
2017-07-28 07:01:31 +0200

    (1) update callMethods[] (2) force using C_routine and no dynamic search to accelerate entry and to avoid accidental use of internal C routines

--- src/bioqc.c
+++ src/bioqc.c
@@ -3,13 +3,24 @@
 
 static const R_CallMethodDef callMethods[] = {
   CALLMETHOD_DEF("gini_matrix", 3),
-  CALLMETHOD_DEF("read_gmt", 1),
+  CALLMETHOD_DEF("gini_numeric", 2),
   CALLMETHOD_DEF("wmw_test", 3),
   CALLMETHOD_DEF("signed_wmw_test", 3),
+  // CALLMETHOD_DEF("read_gmt", 1),
   {NULL, NULL, 0}
 };
 
 
 void R_init_BioQC(DllInfo *info) {
   R_registerRoutines(info, NULL, callMethods, NULL, NULL);
+  /* the line below says that the DLL is not to be searched
+   * for entry points specified by character strings so
+   * .C etc calls will only find registered symbols
+   */  
+  R_useDynamicSymbols(info, FALSE); 
+  /* R_forceSymbols call only allows .C etc calls which 
+   * specify entry points by R objects such as C_routineName
+   * (and not by character strings)
+   */ 
+  R_forceSymbols(info, TRUE);
 }
