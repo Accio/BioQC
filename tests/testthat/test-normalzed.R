632f92c5c4fe64dae193756b8300dffd07548b4a
Iakov Davydov <dav05.gith@myths.ru>
2020-12-08 14:53:59 +0100

    Fix inverted r and f scores (#26)
    * use wilcox.text W statistic as a reference in normalized test
    
    * produce non-inverted r and f scores
    
    account for inverted uStat
    
    Co-authored-by: Iakov Davydov <iakov.davydov@roche.com>

--- tests/testthat/test-normalzed.R
+++ tests/testthat/test-normalzed.R
@@ -8,9 +8,9 @@ rd <- c(rnorm(n1), rnorm(n2, -0.2))
 ind <- c(rep(FALSE, n1), rep(TRUE, n2))
 
 # one-dimensional vector
-wmw_U <- wmwTest(rd, ind, valType = "U")
-wmw_U_f <- wmw_U / n1 / n2
-wmw_U_r <- 2 * wmw_U / n1 / n2 - 1
+U <- wilcox.test(rd[ind], rd[!ind])$statistic
+U_f <- U / n1 / n2
+U_r <- 2 * U / n1 / n2 - 1
 wmw_r <- wmwTest(rd, ind, valType = "r")
 wmw_f <- wmwTest(rd, ind, valType = "f")
 
@@ -18,9 +18,9 @@ wmw_f <- wmwTest(rd, ind, valType = "f")
 ## matrix form
 rmat <- matrix(c(rd, rd + rnorm(length(rd)), rd + rnorm(length(rd))), ncol = 3, byrow = FALSE)
 
-wmw_mat_U <- wmwTest(rmat, ind, valType = "U")
-wmw_mat_U_f <- wmw_mat_U / n1 / n2
-wmw_mat_U_r <- 2 * wmw_mat_U / n1 / n2 - 1
+mat_U <- apply(rmat, 2, function(v) wilcox.test(v[ind], v[!ind])$statistic)
+mat_U_f <- mat_U / n1 / n2
+mat_U_r <- 2 * mat_U / n1 / n2 - 1
 wmw_mat_r <- wmwTest(rmat, ind, valType = "r")
 wmw_mat_f <- wmwTest(rmat, ind, valType = "f")
 
@@ -30,11 +30,11 @@ test_that("Common language effect size and rank-biserial correlation values are 
 })
 
 test_that("Common language effect size corresponds to U value", {
-  expect_equivalent(wmw_f, wmw_U_f)
-  expect_equivalent(wmw_mat_f, wmw_mat_U_f)
+  expect_equivalent(wmw_f, U_f)
+  expect_equivalent(wmw_mat_f, mat_U_f)
 })
 
 test_that("Rank-biserial correlation corresponds to U value", {
-  expect_equivalent(wmw_r, wmw_U_r)
-  expect_equivalent(wmw_mat_r, wmw_mat_U_r)
+  expect_equivalent(wmw_r, U_r)
+  expect_equivalent(wmw_mat_r, mat_U_r)
 })
